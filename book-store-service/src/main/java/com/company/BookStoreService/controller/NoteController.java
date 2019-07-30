00b763c88b393b2a1fa054e740e9a44c4c233894
Mark <m.g.pinho88@gmail.com>
2019-07-30 10:36:59 -0400

    Service layer and controller updates

--- book-store-service/src/main/java/com/company/BookStoreService/controller/NoteController.java
+++ book-store-service/src/main/java/com/company/BookStoreService/controller/NoteController.java
@@ -1,6 +1,8 @@
 package com.company.BookStoreService.controller;
 
-import com.company.BookStoreService.model.Book;
+import com.company.BookStoreService.model.NoteViewModel;
+import com.company.BookStoreService.service.ServiceLayer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,45 +14,55 @@ import java.util.List;
 @RequestMapping("/notes") //Mapping to the "book" URI
 public class NoteController {
 
+    @Autowired
+    ServiceLayer service;
+
     //Create a new note
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Book addNote(@RequestBody Book book) { //TODO - Book needs to be updated to View Model
-        return null;
+    public NoteViewModel addNote(@RequestBody NoteViewModel note) {
+        return service.addNote(note);
     }
 
     //Get a note
     @GetMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public Book getNoteById(@PathVariable("id") int noteId) { //TODO - Book needs to be updated to View Model
-        return null;
+    public NoteViewModel getNoteById(@PathVariable("id") int noteId) {
+        return service.getNote(noteId);
     }
 
     //Get all notes by book
     @GetMapping("/book/book_id")
     @ResponseStatus(HttpStatus.OK)
-    public List<Book> getNotesByBook(@PathVariable("book_id") int bookId) { //TODO - Book needs to be updated to View Model
-        return null;
+    public List<NoteViewModel> getNotesByBook(@PathVariable("book_id") int bookId) {
+        return service.getNotesByBook(bookId);
     }
 
     //Get all notes
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public List<Book> getAllNotes() { //TODO - Book needs to be updated to View Model
-        return null;
+    public List<NoteViewModel> getAllNotes() {
+        return service.getAllNotes();
     }
 
     //Update a note
     @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void updateNote(@PathVariable("id") int noteId, Book book) { //TODO - Book needs to be updated to View Model
+    public void updateNote(@PathVariable("id") int noteId, NoteViewModel note) {
+        if (note.getId() == 0) {
+            note.setId(noteId);
+        }
+        if (note.getId() != noteId) {
+            throw new IllegalArgumentException("Note ID in the path does not match the Note ID in the request");
+        }
 
+        service.updateNote(noteId, note);
     }
 
     //Delete a note
     @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteNote(@PathVariable("id") int noteId) {
-
+        service.deleteNote(noteId);
     }
 }
--- book-store-service/src/main/java/com/company/BookStoreService/dao/BookDao.java
+++ book-store-service/src/main/java/com/company/BookStoreService/dao/BookDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface BookDao {
 
-    //--Standard CRUD methods---------------------------//
+//--Standard CRUD methods---------------------------//
     //Create a new book
     Book addBook(Book book);
 
